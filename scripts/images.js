86071c5749735b302a287608f79c59a1a96823d6
Jan Moeller <jan.moeller0@gmail.com>
2016-12-03 20:34:51 +0100

    Add image manipulation script

---
+++ scripts/images.js
@@ -0,0 +1,30 @@
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const klaw = require('klaw');
+var through2 = require('through2');
+var Jimp = require("jimp");
+var util = require('util');
+var path = require('path');
+
+var excludeDirFilter = through2.obj(function (item, enc, next) {
+  if (!item.stats.isDirectory()) this.push(item)
+  next()
+})
+
+klaw('src/public/img')
+  .pipe(excludeDirFilter)
+  .on('data', function (file) {
+    Jimp.read(file.path).then(function (image) {
+      console.log(path.basename(file.path).split('.')); 
+      // item.resize(256, 256) 
+      //   .write(); 
+    });
+  });
+
+imagemin(['src/public/img/*.{jpg,png}'], 'dist/public/img', {
+  plugins: [
+    imageminMozjpeg(),
+    imageminPngquant({quality: '65-80'})
+  ]
+})
