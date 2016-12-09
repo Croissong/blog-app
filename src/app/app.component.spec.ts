7631334f0a1512ef116f3c8311a737e3fb833780
Jan Moeller <jan.moeller0@gmail.com>
2016-12-09 22:33:43 +0100

    Remove old tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,21 @@
-import {
-  inject,
-  TestBed
-} from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 // Load the implementations that should be tested
 import { AppComponent } from './app.component';
-import { AppState } from './app.service';
 
 describe('App', () => {
+
   // provide our implementations or mocks to the dependency injector
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AppState,
-      AppComponent
-    ]}));
-
-  it('should have a url', inject([ AppComponent ], (app: AppComponent) => {
-    expect(app.url).toEqual('https://twitter.com/AngularClass');
-  }));
-
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ AppComponent ]
+    });
+  
+    /* it('app should replace loader', inject(
+     *   [ AppComponent ], (app: AppComponent) => {
+     *     app.activate();
+     *     expect(el.previousElementSibling.tagName).toEqual('SVG');
+     *     expect(el.previousElementSibling.classList.contains('inactive')).toEqual(true);
+     *   }));*/
+  });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -10,10 +10,7 @@ import {
 } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 
-// Load the implementations that should be tested
-import { AppState } from '../app.service';
 import { HomeComponent } from './home.component';
-import { Title } from './title';
 
 describe('Home', () => {
   // provide our implementations or mocks to the dependency injector
@@ -27,27 +24,13 @@ describe('Home', () => {
           return new Http(backend, defaultOptions);
         },
         deps: [MockBackend, BaseRequestOptions]
-      },
-      AppState,
-      Title,
+      }, 
       HomeComponent
     ]
   }));
-
-  it('should have default data', inject([ HomeComponent ], (home: HomeComponent) => {
-    expect(home.localState).toEqual({ value: '' });
-  }));
-
-  it('should have a title', inject([ HomeComponent ], (home: HomeComponent) => {
-    expect(!!home.title).toEqual(true);
-  }));
-
-  it('should log ngOnInit', inject([ HomeComponent ], (home: HomeComponent) => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
-
-    home.ngOnInit();
-    expect(console.log).toHaveBeenCalled();
-  }));
+  
+  /* it('', inject([ HomeComponent ], (home: HomeComponent) => {
+   *   
+   * }));*/
 
 });
