6722ad2cc88effb15d37677fef4a77958928d51f
Jan Moeller <jan.moeller0@gmail.com>
2016-12-10 13:42:36 +0100

    Try to resolve everything from src / app
    Currently not working for app

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -65,7 +65,7 @@ module.exports = function (options) {
       extensions: ['.ts', '.js', '.json'],
 
       // An array of directory names to be resolved to the current directory
-      modules: [helpers.root('src'), helpers.root('node_modules')],
+      modules: ['src/app', 'src', 'node_modules']
 
     },
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, ViewEncapsulation, AfterViewInit } from '@angular/core';
 import { ROUTES } from './app.routes';
-import '../init.css';
+import 'init.css';
 
 @Component({
   selector: 'app',
